aa36bf3bf02196900c98e27a7637ba1223e09776
Duglas Rodriguez <drodriguez1381@gmail.com>
2025-10-07 17:28:42 -0500

    migraciones de Auth

--- auths/admin.py
+++ auths/admin.py
@@ -1,4 +1,10 @@
 from django.contrib import admin
 from .models import Auth
 admin.site.register(Auth)
-# Register your models here.
+
+
+admin.site.site_header = 'Nombre personalizado'
+admin.site.index_title = 'Panel de control'
+admin.site.site_title = 'Título en la pestaña del navegador'
+
+
