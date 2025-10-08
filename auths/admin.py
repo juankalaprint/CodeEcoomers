03ff19f154fa20a7e883dc209dbeae190ea6474c
Duglas Rodriguez <drodriguez1381@gmail.com>
2025-10-07 21:06:11 -0500

    cambios clases

--- auths/admin.py
+++ auths/admin.py
@@ -3,8 +3,6 @@ from .models import Auth
 admin.site.register(Auth)
 
 
-admin.site.site_header = 'Nombre personalizado'
-admin.site.index_title = 'Panel de control'
-admin.site.site_title = 'Título en la pestaña del navegador'
+# Register your models here.
 
 
--- categorias/models.py
+++ categorias/models.py
@@ -1,3 +1,5 @@
 from django.db import models
 
 # Create your models here.
+Class Categoria(models.Model):
+    nombre_Categoria = models.CharField(max_length=50, unique=True)
