fb1d93c8a05c8f9db6cb4380f976ce7468ab286c
Duglas Rodriguez <drodriguez1381@gmail.com>
2025-10-13 18:05:40 -0500

    cambios 15

--- proyecto_CODE_LATIN/settings.py
+++ proyecto_CODE_LATIN/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -87,7 +88,14 @@ AUTH_USER_MODEL = 'accounts.Auth'  # modelo de usuario personalizado
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join (BASE_DIR,  'db.sqlite3'),
+    #'default': {
+        #'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        #'NAME':'Services' ,
+        #'USER': 'postgres',
+        #'PASSWORD': '1234',
+        #'HOST': 'localhost',
+        #'DATABASE_PORT': '5432',
     }
 }
 
